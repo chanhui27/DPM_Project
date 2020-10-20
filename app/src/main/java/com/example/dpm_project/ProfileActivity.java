8776a5b9c2f94caa74416932ff9c9b9c07d4098e
chanhui27 <chanhui27@users.noreply.github.com>
2020-10-21 09:12:30 +1300

    logo changed

--- app/src/main/java/com/example/dpm_project/ProfileActivity.java
+++ app/src/main/java/com/example/dpm_project/ProfileActivity.java
@@ -1,9 +1,13 @@
 package com.example.dpm_project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
     private EditText editText_Id;
@@ -25,6 +29,29 @@ public class ProfileActivity extends AppCompatActivity {
         editText_Phone = findViewById(R.id.edit_phone);
         editText_Pathway = findViewById(R.id.edit_pathway);
 
-
     }
+
+    public void openDialog(View view) {
+        AlertDialog.Builder alertDialoguilder = new AlertDialog.Builder(this);
+        alertDialoguilder.setMessage(R.string.disclamer);
+
+        alertDialoguilder.setPositiveButton("ACCEPT", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(ProfileActivity.this,"you accepted it", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertDialoguilder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialoguilder.create();
+        alertDialog.show();
+    }
+
+
 }
