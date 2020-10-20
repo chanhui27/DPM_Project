8b758da5eb83b25c7527cc4d8ceb2f46dea0eff3
InteusVII <thatsabevan@live.com>
2020-10-21 11:38:06 +1300

    IMPLEMENTING MENU ON MAIN

--- app/src/main/java/com/example/dpm_project/MainActivity.java
+++ app/src/main/java/com/example/dpm_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dpm_project;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,6 +12,7 @@ import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,25 @@ public class MainActivity extends AppCompatActivity {
 
         //toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                //R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //drawer.addDrawerListener(toggle);
+        //toggle.syncState();
+        /*COMMENTED OUT FOR NO ERRORS FOR PUSH */
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
     }
 
     //popup disclaimer
