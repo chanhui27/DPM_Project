d608b6b973a0d60f2db758964ad379de3b62d613
chanhui27 <chanhui27@users.noreply.github.com>
2020-10-28 08:16:59 +1300

    not sure...

--- app/src/main/java/com/example/dpm_project/MainActivity.java
+++ app/src/main/java/com/example/dpm_project/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.dpm_project;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -14,15 +11,15 @@ import android.view.MenuItem;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
+
 public class MainActivity extends AppCompatActivity {
-   // private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //openDisclaimer();
+        openDisclaimer();
 
         //toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
--- app/src/main/java/com/example/dpm_project/SplashScreen.java
+++ app/src/main/java/com/example/dpm_project/SplashScreen.java
@@ -5,10 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
-    // Duration of wait **/
+    /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,29 +15,24 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
 
-       /* // New Handler to start the Home-Activity
-         // and close this Splash-Screen after some seconds.
+
+        /* New Handler to start the Home-Activity
+         * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                // Create an Intent that will start the Home-Activity.
-                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                /* Create an Intent that will start the Home-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, HomeActivity.class);
                 startActivity(mainIntent);
                 finish();
             }
-        }, SPLASH_DISPLAY_LENGTH);*/
+        }, SPLASH_DISPLAY_LENGTH);
 
-       Intent newintent = new Intent(SplashScreen.this, MainActivity.class);
-       startActivity(newintent);/*
-       Handler hadler=new Handler();
-        hadler.postDelayed(new Runnable() {
-            @Override
-            public void run () {
-                finish();
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(i);
-            }
-        }, 5000);*/
+
+
+
     }
+
+
 
 }
